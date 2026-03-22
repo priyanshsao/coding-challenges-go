4c24ef215519015c75e4d134e6c4c7ffab5efa56
Priyansh Sao <142511810+priyanshsao@users.noreply.github.com>
2026-03-22 18:09:01 +0530

    Revert "feat(challenge-01): add support for command line option '-m'"

--- challenge-01/main.go
+++ challenge-01/main.go
@@ -8,7 +8,6 @@ import (
 	"log"
 	"os"
 	"unicode"
-	"unicode/utf8"
 )
 
 type FileInfo struct {
@@ -53,7 +52,6 @@ func main() {
 	}
 
 	buffer := make([]byte, 32*1024)
-	leftOver := []byte{}
 	inWord := false
 
 	for {
@@ -74,7 +72,6 @@ func main() {
 		if getWords {
 			processWords(buffer[:n], myFile, &inWord)
 		}
-		if getRunes {processRunes(buffer[:n], myFile, &leftOver)}
 	}
 
 	if getBytes {
@@ -112,24 +109,5 @@ func processWords(buffer []byte, file *FileInfo, inWord *bool) {
 				*inWord = true
 			}
 		}
-	}
-}
-
-func processRunes(buffer []byte, file *FileInfo, leftOver *[]byte) {
-
-	buffer = append(*leftOver, buffer...)
-	*leftOver = (*leftOver)[:0]
-
-	i := 0
-	for i < len(buffer) {
-		if !utf8.FullRune(buffer[i:]) {
-			*leftOver = append(*leftOver, buffer[i:]...)
-			break
-		}
-		// need to add error check for utf8.RuneError
-		_, size := utf8.DecodeRune(buffer[i:])
-		
-		file.Runes++
-		i += size
 	}
 }
