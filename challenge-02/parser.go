dd3c261320b7c188b38971d217991785ab841a03
Priyansh Sao <saopriyansh06@gmail.com>
2026-04-04 20:37:32 +0530

    feat(challenge-02/parser): add parser implementation(#49)
    Signed-off-by: Priyansh Sao <saopriyansh06@gmail.com>

---
+++ challenge-02/parser.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Parser struct {
+	lexer        *Lexer
+	currentToken Token
+	nextToken    Token
+}
+
+func NewParser(input []byte) *Parser {
+	parser := new(Parser)
+	lexer := NewLexer(input)
+
+	parser.lexer = lexer
+	parser.currentToken = lexer.NextToken()
+	parser.nextToken = lexer.NextToken()
+
+	return parser
+}
+
+func (p *Parser) Move() {
+	p.currentToken = p.nextToken
+	p.nextToken = p.lexer.NextToken()
+}
+
+func (p *Parser) Parse() (interface{}, error) {
+	parsedObject, err := p.ParseToken()
+	if err != nil {
+		return nil, err
+	}
+
+	p.Move()
+
+	if p.currentToken.Type != EOF {
+		return nil, fmt.Errorf("expected } but got %s at line %d", p.currentToken.Literal, p.lexer.line)
+	}
+
+	return parsedObject, nil
+}
+
+func (p *Parser) ParseToken() (interface{}, error) {
+	var parsedValue interface{}
+	var err error
+
+	switch p.currentToken.Type {
+	case BeginObject:
+		parsedValue, err = p.ParseObject()
+		if err != nil {
+			return nil, err
+		}
+	case EOF:
+		return nil, fmt.Errorf("unexpected end of input at line %d", p.lexer.line)
+	default:
+		return nil, fmt.Errorf("unknown token %s at line %d", p.currentToken.Literal, p.lexer.line)
+	}
+
+	return parsedValue, nil
+}
+
+func (p *Parser) ParseObject() (interface{}, error) {
+	object := make(map[string]interface{})
+
+	if p.nextToken.Type != EndObject {
+		return nil, fmt.Errorf("unexpected token %s after parsing object at line %d", p.currentToken.Literal, p.lexer.line)
+	}
+
+	p.Move()
+
+	return object, nil
+}
