5ec0190bf1abd9c6a7c44f6df4ac361bfe6c81cf
Priyansh Sao <saopriyansh06@gmail.com>
2026-04-16 12:55:51 +0530

    feat(tokens): add String function in tokens.go
    Signed-off-by: Priyansh Sao <saopriyansh06@gmail.com>

--- challenge-02/tokens.go
+++ challenge-02/tokens.go
@@ -31,3 +31,30 @@ type Token struct {
 	Type    tokenType
 	Literal string
 }
+
+// For tests to get the string value of token's type
+func (tt tokenType) String() string {
+
+	switch tt {
+	case BeginObject:
+		return "BeginObject"
+	case EndObject:
+		return "EndObject"
+	case BeginArray:
+		return "BeginArray"
+	case EndArray:
+		return "EndArray"
+	case NameSeparator:
+		return "NameSeperator"
+	case ValueSeparator:
+		return "ValueSeperator"
+	case Number:
+		return "Number"
+	default:
+		if tt == True || tt == False || tt == Null {
+			return "Literal"
+		}
+
+		return "Unknown"
+	}
+}
