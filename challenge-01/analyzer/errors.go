f55bc08fbff9bd1a9634c3e56062e995431e0e3e
Priyansh Sao <saopriyansh06@gmail.com>
2026-05-24 17:24:23 +0530

    refactor(analyzer): separate error definition
    Signed-off-by: Priyansh Sao <saopriyansh06@gmail.com>

---
+++ challenge-01/analyzer/errors.go
@@ -0,0 +1,10 @@
+package analyzer
+
+import "errors"
+
+var (
+	// ErrEmptyFilePath indicates, provided file path is empty.
+	ErrEmptyFilePath error = errors.New("empty file path.")
+	// ErrNoArgProvided indicates, no argument is provided.
+	ErrNoArgProvided error = errors.New("no argument provided.")
+)
