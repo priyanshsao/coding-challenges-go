270778a54782f61cb1cee57a6274ad5da2a62a4f
Priyansh Sao <saopriyansh06@gmail.com>
2026-05-29 17:34:55 +0530

    feat: rename config in analyzer.go
    Signed-off-by: Priyansh Sao <saopriyansh06@gmail.com>

--- challenge-01/analyzer/analyzer.go
+++ challenge-01/analyzer/analyzer.go
@@ -14,7 +14,7 @@ func Process(c *stdconfig.Config) error {
 	buffer := make([]byte, 32*1024) //32kB
 	leftOver := []byte{}
 	inWord := false
-	reader := config.Reader
+	reader := c.Reader
 
 	for {
 		n, err := reader.Read(buffer)
@@ -25,19 +25,19 @@ func Process(c *stdconfig.Config) error {
 			return err
 		}
 
-		for opts, val := range config.Opts {
+		for opts, val := range c.Opts {
 
 			if val {
 
 				switch opts {
 				case stdconfig.BYTES:
-					processBytes(buffer[:n], &config.Result)
+					processBytes(buffer[:n], &c.Result)
 				case stdconfig.LINES:
-					processLines(buffer[:n], &config.Result)
+					processLines(buffer[:n], &c.Result)
 				case stdconfig.WORDS:
-					processWords(buffer[:n], &config.Result, &inWord)
+					processWords(buffer[:n], &c.Result, &inWord)
 				case stdconfig.RUNES:
-					processRunes(buffer[:n], &config.Result, &leftOver)
+					processRunes(buffer[:n], &c.Result, &leftOver)
 				}
 			}
 		}
