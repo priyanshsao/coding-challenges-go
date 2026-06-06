6959fe527ec3003e1ba4afb3240b32270c832708
Priyansh Sao <saopriyansh06@gmail.com>
2026-06-06 15:41:26 +0530

    fix(tests): update broken tests

--- challenge-02/internal/lexer/lexer_test.go
+++ challenge-02/internal/lexer/lexer_test.go
@@ -1,13 +1,15 @@
-package main
+package lexer
 
 import (
 	"reflect"
 	"testing"
+
+	"github.com/priyanshsao/coding-challenges-go/challenge-02/internal/define"
 )
 
 func Test_NewLexer(t *testing.T) {
 	input := []byte(`{"Key":"value"}`)
-	l := NewLexer(input)
+	l := New(input)
 
 	if l.char != '{' {
 		t.Errorf("expected '%c' but got '%c'", input[0], l.char)
@@ -68,7 +70,7 @@ func Test_Read(t *testing.T) {
 				next:     tt.next,
 				line:     tt.line,
 			}
-			l.Read()
+			l.read()
 
 			if l.char != tt.expected.char {
 				t.Errorf("expected '%v' but got '%v'", tt.expected.char, l.char)
@@ -106,8 +108,8 @@ func Test_LookNext(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			l := NewLexer([]byte(tt.input))
-			got := l.LookNext()
+			l := New([]byte(tt.input))
+			got := l.lookNext()
 
 			if got != tt.expected {
 				t.Errorf("expected '%c' but got '%c'", tt.expected, got)
@@ -120,31 +122,31 @@ func Test_NextToken(t *testing.T) {
 	type test struct {
 		name     string
 		input    string
-		expected []Token
+		expected []define.Token
 	}
 
 	tests := []test{
 		{
 			name:  "Should return exact tokens for all base case",
 			input: `[]{}:,"HelLo"12trueF`,
-			expected: []Token{
-				{BeginArray, "["},
-				{EndArray, "]"},
-				{BeginObject, "{"},
-				{EndObject, "}"},
-				{NameSeparator, ":"},
-				{ValueSeparator, ","},
-				{String, "HelLo"},
-				{Number, "12"},
-				{True, "true"},
-				{Illegal, "F"},
+			expected: []define.Token{
+				{define.BeginArray, "["},
+				{define.EndArray, "]"},
+				{define.BeginObject, "{"},
+				{define.EndObject, "}"},
+				{define.NameSeparator, ":"},
+				{define.ValueSeparator, ","},
+				{define.String, "HelLo"},
+				{define.Number, "12"},
+				{define.True, "true"},
+				{define.Illegal, "F"},
 			},
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			l := NewLexer([]byte(tt.input))
+			l := New([]byte(tt.input))
 
 			for i := 0; i < len(tt.expected); i++ {
 				token := l.NextToken()
@@ -161,31 +163,32 @@ func Test_ReadString(t *testing.T) {
 	type test struct {
 		name     string
 		input    string
-		expected Token
+		expected define.Token
 	}
 
 	tests := []test{
 		{
 			name:     "returns token for simple string",
 			input:    `"key"`,
-			expected: Token{String, "key"},
+			expected: define.Token{define.String, "key"},
 		},
 		{
 			name:     "returns token for spaced string with space in literal",
 			input:    `"   key"`,
-			expected: Token{String, "   key"},
+			expected: define.Token{define.String, "   key"},
 		},
 		{
 			name:     "returns Illegal token for unclosed string",
 			input:    `"key`,
-			expected: Token{Illegal, "key"},
+			expected: define.Token{define.Illegal, "key"},
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			l := NewLexer([]byte(tt.input))
-			token := l.ReadString()
+			l := New([]byte(tt.input))
+
+			token := l.readString()
 
 			if token.Type != tt.expected.Type {
 				t.Fatalf("expected `%s` token type got `%s`", tt.expected.Type, token.Type)
@@ -202,79 +205,79 @@ func Test_ReadNum(t *testing.T) {
 	type test struct {
 		name     string
 		input    string
-		expected Token
+		expected define.Token
 	}
 
 	tests := []test{
 		{
 			name:  "Should return token for positive int",
 			input: "9004",
-			expected: Token{
-				Number,
-				"9004",
+			expected: define.Token{
+				Type:    define.Number,
+				Literal: "9004",
 			},
 		},
 		{
 			name:  "Should return token for negative int",
 			input: "-236",
-			expected: Token{
-				Number,
-				"-236",
+			expected: define.Token{
+				Type:    define.Number,
+				Literal: "-236",
 			},
 		},
 		{
 			name:  "Should return token for negative decimal number",
 			input: "+23",
-			expected: Token{
-				Illegal,
-				"+",
+			expected: define.Token{
+				Type:    define.Illegal,
+				Literal: "+",
 			},
 		},
 		{
 			name:  "Should return illegal token for non-number",
 			input: "a",
-			expected: Token{
-				Illegal,
-				"a",
+			expected: define.Token{
+				Type:    define.Illegal,
+				Literal: "a",
 			},
 		},
 		{
 			name:  "Should return token upto right position",
 			input: "4-23.6",
-			expected: Token{
-				Number,
-				"4",
+			expected: define.Token{
+				Type:    define.Number,
+				Literal: "4",
 			},
 		},
 		{
 			name:  "Should return illegal token for no digit after '.'",
 			input: "423.",
-			expected: Token{
-				Illegal,
-				"423.",
+			expected: define.Token{
+				Type:    define.Illegal,
+				Literal: "423.",
 			},
 		},
 		{
 			name:  "Should return illegal token for repititive '.'",
 			input: "423..8",
-			expected: Token{
-				Illegal,
-				"423.",
+			expected: define.Token{
+				Type:    define.Illegal,
+				Literal: "423.",
 			},
 		},
 		{
 			name:  "Should return token upto valid digits",
 			input: "4.23.8",
-			expected: Token{
-				Number,
-				"4.23",
+			expected: define.Token{
+				Type:    define.Number,
+				Literal: "4.23",
 			},
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			l := NewLexer([]byte(tt.input))
+			l := New([]byte(tt.input))
 
 			prevPos := l.position
 			numToken := l.NextToken()
@@ -295,39 +298,39 @@ func Test_ReadLiteral(t *testing.T) {
 	type test struct {
 		name     string
 		input    string
-		expected Token
+		expected define.Token
 	}
 
 	tests := []test{
 		{
 			name:  "Should return token for simple literal",
 			input: "true",
-			expected: Token{
-				True,
+			expected: define.Token{
+				define.True,
 				"true",
 			},
 		},
 		{
 			name:  "Should return illegal token for capitalized literal",
 			input: "TRUE",
-			expected: Token{
-				Illegal,
+			expected: define.Token{
+				define.Illegal,
 				"T",
 			},
 		},
 		{
 			name:  "Should return illegal token for mix cased literal",
 			input: "tRUE",
-			expected: Token{
-				Illegal,
+			expected: define.Token{
+				define.Illegal,
 				"t",
 			},
 		},
 		{
 			name:  "Should return illegal token for wrong literal",
 			input: "tru",
-			expected: Token{
-				Illegal,
+			expected: define.Token{
+				define.Illegal,
 				"tru",
 			},
 		},
@@ -335,7 +338,7 @@ func Test_ReadLiteral(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			l := NewLexer([]byte(tt.input))
+			l := New([]byte(tt.input))
 
 			prevPos := l.position
 			token := l.NextToken()
