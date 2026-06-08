1d918fa3f8f382ae0704bece63cbe2493e7423eb
Priyansh Sao <saopriyansh06@gmail.com>
2026-06-08 13:00:20 +0530

    feat(main): add testing using tests folder
    Signed-off-by: Priyansh Sao <saopriyansh06@gmail.com>

--- challenge-02/cmd/main.go
+++ challenge-02/cmd/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"path/filepath"
 
 	"github.com/priyanshsao/coding-challenges-go/challenge-02/json"
 	"github.com/sirupsen/logrus"
@@ -11,27 +12,27 @@ import (
 
 func main() {
 
+	const testDir = "./tests"
+
 	setLogger()
 
-	path := "./tests/step2/invalid2.json"
-	file, err := os.Open(path)
+	dirs, err := processDir(testDir)
 	if err != nil {
 		logrus.Error(err)
 		return
 	}
-	defer file.Close()
 
-	fileData, _ := io.ReadAll(file)
+	for _, dir := range dirs {
 
-	parser := json.NewParser(fileData)
+		path := filepath.Join(testDir, dir.Name())
+		files, err := processDir(path)
+		if err != nil {
+			logrus.Error(err)
+			return
+		}
 
-	res, err := parser.Parse()
-	if err != nil {
-		logrus.Errorf("unable to parse: %v", err)
-		return
+		process(files, path)
 	}
-
-	fmt.Println(res)
 }
 
 func setLogger() {
@@ -44,4 +45,52 @@ func setLogger() {
 
 	// set log level to debug.
 	logrus.SetLevel(logrus.DebugLevel)
+}
+
+func processDir(path string) ([]os.DirEntry, error) {
+
+	dirs, err := os.ReadDir(path)
+	if err != nil {
+
+		return dirs, err
+	}
+
+	return dirs, nil
+}
+
+func process(files []os.DirEntry, root string) {
+
+	for _, file := range files {
+
+		if file.IsDir() {
+			continue
+		}
+
+		if filepath.Ext(file.Name()) != ".json" {
+			continue
+		}
+
+		path := filepath.Join(root, file.Name())
+
+		fmt.Printf("\n-----------------[%v]-----------------\n\n", path)
+
+		file, err := os.Open(path)
+		if err != nil {
+			logrus.Error(err)
+			continue
+		}
+		defer file.Close()
+
+		fileData, _ := io.ReadAll(file)
+
+		parser := json.NewParser(fileData)
+
+		res, err := parser.Parse()
+		if err != nil {
+			logrus.Errorf("unable to parse: %v", err)
+			continue
+		}
+
+		logrus.Info(res)
+	}
 }
